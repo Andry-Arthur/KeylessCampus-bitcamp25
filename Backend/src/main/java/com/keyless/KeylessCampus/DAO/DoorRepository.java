66dbd7035ffd0018cede8885cc27fea14c01be1e
PrabeshPP <prabesh.bista369@gmail.com>
2025-04-12 01:43:20 -0400

    repo

---
+++ Backend/src/main/java/com/keyless/KeylessCampus/DAO/DoorRepository.java
@@ -0,0 +1,11 @@
+package com.keyless.KeylessCampus.DAO;
+
+import com.keyless.KeylessCampus.model.DoorSystem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.rmi.server.UID;
+
+@Repository
+public interface DoorRepository extends JpaRepository<DoorSystem, UID> {
+}
---
+++ Backend/src/main/java/com/keyless/KeylessCampus/DAO/UserRepository.java
@@ -0,0 +1,9 @@
+package com.keyless.KeylessCampus.DAO;
+
+import com.keyless.KeylessCampus.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.rmi.server.UID;
+
+public interface UserRepository extends JpaRepository<User, UID> {
+}

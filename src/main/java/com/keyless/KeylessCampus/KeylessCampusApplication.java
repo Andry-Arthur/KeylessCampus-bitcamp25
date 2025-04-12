3783bc1eb175d86e1e95cea383f7162a256d6a5c
PrabeshPP <prabesh.bista369@gmail.com>
2025-04-11 22:19:03 -0400

    [added] keyless backend proj

---
+++ src/main/java/com/keyless/KeylessCampus/KeylessCampusApplication.java
@@ -0,0 +1,13 @@
+package com.keyless.KeylessCampus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeylessCampusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeylessCampusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/keyless/KeylessCampus/KeylessCampusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keyless.KeylessCampus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeylessCampusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
